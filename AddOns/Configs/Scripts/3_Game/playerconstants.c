aa86c157cb39f50314e5ce7fed79b385377140bd
Streatman <TheStreatman@gmail.com>
2020-11-29 16:13:02 +0100

    unrealised proposal

--- AddOns/Configs/Scripts/3_Game/playerconstants.c
+++ AddOns/Configs/Scripts/3_Game/playerconstants.c
@@ -4,6 +4,14 @@ modded class PlayerConstants
 	static const float SL_ENERGY_LOW = 500;
 	static const float SL_ENERGY_NORMAL = 3000;
 	static const float SL_ENERGY_HIGH = 9000;
+
+	static const float LOW_ENERGY_THRESHOLD = 500;
+	
+//	static const float METABOLIC_SPEED_ENERGY_WALK = 0.1;	//Vanilla 0.08
+//	static const float METABOLIC_SPEED_ENERGY_JOG = 0.5;	//Vanilla 0.3
+//	static const float METABOLIC_SPEED_ENERGY_SPRINT = 0.9;	//Vanilla 0.6
+	
+	//--------------------------------------------------------
 	
 	static const float SL_WATER_CRITICAL = 0;
 	static const float SL_WATER_LOW = 500;
@@ -11,5 +19,4 @@ modded class PlayerConstants
 	static const float SL_WATER_HIGH = 4500;
 
 	static const float LOW_WATER_THRESHOLD = 500;
-	static const float LOW_ENERGY_THRESHOLD = 500;
 }
