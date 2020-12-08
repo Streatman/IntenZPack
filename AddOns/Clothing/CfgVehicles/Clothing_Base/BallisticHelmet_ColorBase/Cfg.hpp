009d9455227817bd222ab377b8ea0b15171c3797
Streatman <TheStreatman@gmail.com>
2020-12-08 20:31:20 +0100

    BallisticHelmet

---
+++ AddOns/Clothing/CfgVehicles/Clothing_Base/BallisticHelmet_ColorBase/Cfg.hpp
@@ -0,0 +1,20 @@
+class BallisticHelmet_ColorBase: Clothing
+{
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
+};
+
+class BallisticHelmet_White: BallisticHelmet_ColorBase
+{
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"IntenZ\Clothing\Assets\Headgear\helmetmich\white_co.paa",
+		"IntenZ\Clothing\Assets\Headgear\helmetmich\white_co.paa",
+		"IntenZ\Clothing\Assets\Headgear\helmetmich\white_co.paa"
+	};
+};
