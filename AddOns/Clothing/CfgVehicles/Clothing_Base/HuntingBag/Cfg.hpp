72d62e2104ad682c33cbac13fd9ad8333cba3ba2
Streatman <TheStreatman@gmail.com>
2020-12-08 20:30:17 +0100

    HuntingBag

---
+++ AddOns/Clothing/CfgVehicles/Clothing_Base/HuntingBag/Cfg.hpp
@@ -0,0 +1,20 @@
+class HuntingBag: Clothing
+{
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
+};
+
+class HuntingBag_White: HuntingBag
+{
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"IntenZ\Clothing\Assets\Backpacks\HuntingBag\white_co.paa",
+		"IntenZ\Clothing\Assets\Backpacks\HuntingBag\white_co.paa",
+		"IntenZ\Clothing\Assets\Backpacks\HuntingBag\white_co.paa"
+	};
+};
