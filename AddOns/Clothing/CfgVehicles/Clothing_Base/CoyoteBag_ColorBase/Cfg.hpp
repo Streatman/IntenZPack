a98ffe9d6beceb6093bca7a517449317d1cdb11f
Streatman <TheStreatman@gmail.com>
2020-12-08 20:30:04 +0100

    Coyotebp

---
+++ AddOns/Clothing/CfgVehicles/Clothing_Base/CoyoteBag_ColorBase/Cfg.hpp
@@ -0,0 +1,20 @@
+class CoyoteBag_ColorBase: Clothing
+{
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
+};
+
+class CoyoteBag_White: CoyoteBag_ColorBase
+{
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"IntenZ\Clothing\Assets\Backpacks\Coyotebp\white_co.paa",
+		"IntenZ\Clothing\Assets\Backpacks\Coyotebp\white_co.paa",
+		"IntenZ\Clothing\Assets\Backpacks\Coyotebp\white_co.paa"
+	};
+};
