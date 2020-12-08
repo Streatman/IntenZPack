93f1ff273d386eaf051147949870c7f05d963144
Streatman <TheStreatman@gmail.com>
2020-12-08 20:31:47 +0100

    Ssh68Helmet

---
+++ AddOns/Clothing/CfgVehicles/Clothing_Base/Ssh68Helmet/Cfg.hpp
@@ -0,0 +1,20 @@
+class Ssh68Helmet: Clothing
+{
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
+};
+
+class Ssh68Helmet_White: Ssh68Helmet
+{
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"IntenZ\Clothing\Assets\Headgear\Ssh68Helmet\white_co.paa",
+		"IntenZ\Clothing\Assets\Headgear\Ssh68Helmet\white_co.paa",
+		"IntenZ\Clothing\Assets\Headgear\Ssh68Helmet\white_co.paa"
+	};
+};
