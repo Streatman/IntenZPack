c42ef00d670e76564ca62eaf20166c57d03a7154
Streatman <TheStreatman@gmail.com>
2020-12-08 20:32:26 +0100

    CombatBoots

---
+++ AddOns/Clothing/CfgVehicles/Clothing_Base/CombatBoots_ColorBase/Cfg.hpp
@@ -0,0 +1,20 @@
+class CombatBoots_ColorBase: Clothing
+{
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
+};
+
+class CombatBoots_Winter: CombatBoots_ColorBase
+{
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"IntenZ\Clothing\Assets\Shoes\Combatboots\Winter_co.paa",
+		"IntenZ\Clothing\Assets\Shoes\Combatboots\Winter_co.paa",
+		"IntenZ\Clothing\Assets\Shoes\Combatboots\Winter_co.paa"
+	};
+};
