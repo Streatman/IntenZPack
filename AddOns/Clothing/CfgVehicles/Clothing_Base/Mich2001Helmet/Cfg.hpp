33bba9bfb2dfa2b73e1bb664b3f7d4d8a48a988a
Streatman <TheStreatman@gmail.com>
2020-12-08 20:31:35 +0100

    Mich2001Helmet

---
+++ AddOns/Clothing/CfgVehicles/Clothing_Base/Mich2001Helmet/Cfg.hpp
@@ -0,0 +1,20 @@
+class Mich2001Helmet: Clothing
+{
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
+};
+
+class Mich2001Helmet_White: Mich2001Helmet
+{
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"IntenZ\Clothing\Assets\Headgear\mich2001\white_co.paa",
+		"IntenZ\Clothing\Assets\Headgear\mich2001\white_co.paa",
+		"IntenZ\Clothing\Assets\Headgear\mich2001\white_co.paa"
+	};
+};
