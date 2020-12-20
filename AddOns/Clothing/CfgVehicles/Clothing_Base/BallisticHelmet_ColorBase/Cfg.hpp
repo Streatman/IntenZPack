3da54c8d9cc305b103009af8e27e78e57b4ade73
Streatman <TheStreatman@gmail.com>
2020-12-20 19:41:40 +0100

    Helmets

--- AddOns/Clothing/CfgVehicles/Clothing_Base/BallisticHelmet_ColorBase/Cfg.hpp
+++ AddOns/Clothing/CfgVehicles/Clothing_Base/BallisticHelmet_ColorBase/Cfg.hpp
@@ -18,3 +18,13 @@ class BallisticHelmet_White: BallisticHelmet_ColorBase
 		"IntenZ\Clothing\Assets\Headgear\helmetmich\white_co.paa"
 	};
 };
+class BallisticHelmet_Black: BallisticHelmet_ColorBase
+{
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"IntenZ\Clothing\Assets\Headgear\helmetmich\black_co.paa",
+		"IntenZ\Clothing\Assets\Headgear\helmetmich\black_co.paa",
+		"IntenZ\Clothing\Assets\Headgear\helmetmich\black_co.paa"
+	};
+};
--- AddOns/Clothing/CfgVehicles/Clothing_Base/Mich2001Helmet/Cfg.hpp
+++ AddOns/Clothing/CfgVehicles/Clothing_Base/Mich2001Helmet/Cfg.hpp
@@ -18,3 +18,13 @@ class Mich2001Helmet_White: Mich2001Helmet
 		"IntenZ\Clothing\Assets\Headgear\mich2001\white_co.paa"
 	};
 };
+class Mich2001Helmet_Black: Mich2001Helmet
+{
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"IntenZ\Clothing\Assets\Headgear\mich2001\black_co.paa",
+		"IntenZ\Clothing\Assets\Headgear\mich2001\black_co.paa",
+		"IntenZ\Clothing\Assets\Headgear\mich2001\black_co.paa"
+	};
+};
