3672e89911e1f4b7d89b19dfd709d76e7ae289c4
Streatman <TheStreatman@gmail.com>
2021-01-10 20:48:56 +0100

    Suppressors

--- AddOns/Configs/CfgVehicles/Inventory_Base/ItemSuppressor/Cfg.hpp
+++ AddOns/Configs/CfgVehicles/Inventory_Base/ItemSuppressor/Cfg.hpp
@@ -10,3 +10,48 @@ class MP5_Compensator: ItemSuppressor
 	displayName="$STR_iz_cfgVehicles_MP5_Compensator0";
 	descriptionShort="$STR_iz_cfgVehicles_MP5_Compensator1";
 };
+class M4_Suppressor: ItemSuppressor
+{
+	repairableWithKits[]={0};
+	noiseShootModifier=-0.95;
+	class DamageSystem
+	{
+		class GlobalHealth
+		{
+			class Health
+			{
+				hitpoints=6;
+			};
+		};
+	};
+};
+class AK_Suppressor: ItemSuppressor
+{
+	repairableWithKits[]={0};
+	noiseShootModifier=-0.95;
+	class DamageSystem
+	{
+		class GlobalHealth
+		{
+			class Health
+			{
+				hitpoints=6;
+			};
+		};
+	};
+};
+class PistolSuppressor: ItemSuppressor
+{
+	repairableWithKits[]={0};
+	noiseShootModifier=-0.95;
+	class DamageSystem
+	{
+		class GlobalHealth
+		{
+			class Health
+			{
+				hitpoints=6;
+			};
+		};
+	};
+};
