bc37c350fd68f04ab47b2be8611c5f85ef1c8b79
Streatman <TheStreatman@gmail.com>
2021-01-11 22:33:05 +0100

    Black BDU

---
+++ AddOns/Clothing/CfgVehicles/Clothing_Base/BDUJacket/Cfg.hpp
@@ -0,0 +1,26 @@
+class BDUJacket: Clothing
+{
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\dz\characters\tops\data\BDU_Jacket_co.paa",
+		"\dz\characters\tops\data\BDU_Jacket_co.paa",
+		"\dz\characters\tops\data\BDU_Jacket_co.paa"
+	};
+};
+
+class BDUJacket_Black: BDUJacket
+{
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"IntenZ\Clothing\Assets\Tops\BDU_Jacket\black_co.paa",
+		"IntenZ\Clothing\Assets\Tops\BDU_Jacket\black_co.paa",
+		"IntenZ\Clothing\Assets\Tops\BDU_Jacket\black_co.paa"
+	};
+};
---
+++ AddOns/Clothing/CfgVehicles/Clothing_Base/BDUPants/Cfg.hpp
@@ -0,0 +1,26 @@
+class BDUPants: Clothing
+{
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\dz\characters\pants\data\bdu_pants_woodland_co.paa",
+		"\dz\characters\pants\data\bdu_pants_woodland_co.paa",
+		"\dz\characters\pants\data\bdu_pants_woodland_co.paa"
+	};
+};
+
+class BDUPants_Black: BDUPants
+{
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"IntenZ\Clothing\Assets\Pants\BDU_Pants\black_co.paa",
+		"IntenZ\Clothing\Assets\Pants\BDU_Pants\black_co.paa",
+		"IntenZ\Clothing\Assets\Pants\BDU_Pants\black_co.paa"
+	};
+};
