dc83b6cd6371a39885aee0ab508d1d8ceb0fad8e
Streatman <TheStreatman@gmail.com>
2021-02-16 00:11:15 +0100

    Map

--- AddOns/Map/CfgWorlds/ChernarusPlus/Cfg.hpp
+++ AddOns/Map/CfgWorlds/ChernarusPlus/Cfg.hpp
@@ -1,10 +1,10 @@
 class ChernarusPlus: CAWorld
 {
-	mapTextureClosed="IntenZ\Map\Assets\map_chernarus_co.paa";
-	mapTextureOpened="IntenZ\Map\Assets\karta_co.paa";
-	mapTextureLegend="IntenZ\Map\Assets\karta_side_co.paa";
+	mapTextureClosed = "IntenZ\Map\Assets\Survivor\map_chernarus_co.paa";
+	mapTextureOpened = "IntenZ\Map\Assets\Survivor\karta_co.paa";
+	mapTextureLegend = "IntenZ\Map\Assets\Survivor\karta_side_co.paa";
 	
-//	mapTextureClosed="dz\gear\navigation\data\map_chernarus_co.paa";
-//	mapTextureOpened="dz\structures\signs\tourist\data\karta_co.paa";
-//	mapTextureLegend="dz\structures\signs\tourist\data\karta_side_co.paa";
+//	mapTextureClosed = "dz\gear\navigation\data\map_chernarus_co.paa";
+//	mapTextureOpened = "dz\structures\signs\tourist\data\karta_co.paa";
+//	mapTextureLegend = "dz\structures\signs\tourist\data\karta_side_co.paa";
 };
