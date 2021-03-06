d672c96cf611c9e1efb37c9ee40333ab181f70df
Streatman <TheStreatman@gmail.com>
2021-03-06 22:10:04 +0100

    Update playerstatspco.c

--- AddOns/Configs/Scripts/4_World/classes/playerstats/playerstatspco.c
+++ AddOns/Configs/Scripts/4_World/classes/playerstats/playerstatspco.c
@@ -5,8 +5,6 @@ modded class PlayerStatsPCO_v115 extends PlayerStatsPCO_Base
 		RegisterStat(EPlayerStats_v115.HEATCOMFORT,	new PlayerStat<float>	(-1,1,0,"HeatComfort",EPSstatsFlags.EMPTY) );
 		RegisterStat(EPlayerStats_v115.TREMOR,		new PlayerStat<float>	(0,1,0,"Tremor",EPSstatsFlags.EMPTY) );
 		RegisterStat(EPlayerStats_v115.WET,			new PlayerStat<int>		(0,1,0,"Wet",EPSstatsFlags.EMPTY) );
-	//	RegisterStat(EPlayerStats_v115.ENERGY,		new PlayerStat<float>	(0,10000,1300,"Energy",EPSstatsFlags.EMPTY) );	//Streatmans proposal
-	//	RegisterStat(EPlayerStats_v115.WATER,		new PlayerStat<float>	(0,5000,1000,"Water",EPSstatsFlags.EMPTY) );	//Streatmans proposal
 		RegisterStat(EPlayerStats_v115.ENERGY,		new PlayerStat<float>	(0,10000,1400,"Energy",EPSstatsFlags.EMPTY) );
 		RegisterStat(EPlayerStats_v115.WATER,		new PlayerStat<float>	(0,5000,1200,"Water",EPSstatsFlags.EMPTY) );
 		RegisterStat(EPlayerStats_v115.DIET,		new PlayerStat<float>	(0,5000,2500,"Diet",EPSstatsFlags.EMPTY) );
