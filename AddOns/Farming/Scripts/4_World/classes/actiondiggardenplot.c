e6efd62929458b747fee5ab89532fdcf9ef75d65
Streatman <TheStreatman@gmail.com>
2021-03-06 22:11:01 +0100

    Update actiondiggardenplot.c

--- AddOns/Farming/Scripts/4_World/classes/actiondiggardenplot.c
+++ AddOns/Farming/Scripts/4_World/classes/actiondiggardenplot.c
@@ -1,15 +1,15 @@
 modded class ActionDigGardenPlot: ActionDeployObject
 {	
 	override void OnFinishProgressServer( ActionData action_data )
-	{	
+	{
 		PlaceObjectActionData poActionData;
 		poActionData = PlaceObjectActionData.Cast(action_data);
 		EntityAI entity_for_placing = action_data.m_MainItem;
 		vector position = action_data.m_Player.GetLocalProjectionPosition();
 		vector orientation = action_data.m_Player.GetLocalProjectionOrientation();
-				
+		
 		if ( GetGame().IsMultiplayer() )
-		{		
+		{
 			m_GardenPlot = GardenPlot.Cast( action_data.m_Player.GetHologramServer().PlaceEntity( entity_for_placing ));
 			m_GardenPlot.SetOrientation( orientation );
 			action_data.m_Player.GetHologramServer().CheckPowerSource();
@@ -17,10 +17,10 @@ modded class ActionDigGardenPlot: ActionDeployObject
 			
 			m_GardenPlot.OnPlacementComplete( action_data.m_Player );
 		}
-			
+		
 		//local singleplayer
 		if ( !GetGame().IsMultiplayer())
-		{						
+		{
 			m_GardenPlot = GardenPlot.Cast( action_data.m_Player.GetHologramLocal().PlaceEntity( entity_for_placing ));
 			m_GardenPlot.SetOrientation( orientation );
 			action_data.m_Player.PlacingCompleteLocal();
@@ -31,10 +31,9 @@ modded class ActionDigGardenPlot: ActionDeployObject
 		GetGame().ClearJuncture( action_data.m_Player, entity_for_placing );
 		action_data.m_MainItem.SetIsBeingPlaced( false );
 		action_data.m_Player.GetSoftSkillsManager().AddSpecialty( m_SpecialtyWeight );
-		poActionData.m_AlreadyPlaced = true;	
+		poActionData.m_AlreadyPlaced = true;
 		action_data.m_MainItem.SoundSynchRemoteReset();
 		
 		MiscGameplayFunctions.DealAbsoluteDmg(action_data.m_MainItem, 150);
-		
 	}
 };
